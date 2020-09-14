1da29a126932603e4251e8f9a8f8121e8c8d1c69
timo500810897 <70717768+timo500810897@users.noreply.github.com>
2020-09-14 18:05:37 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-// JavaScript Document
+/*
+function stripHtml(html) {
+  var tmp = document.createElement("DIV");
+  tmp.innerHTML = html;
+  return tmp.textContent || tmp.innerText || "";
+}
+*/
+
+/*
+function myFunction(x) {
+  if (x.matches) {
+    document.getElementsByClassName("bits").innerHTML = "";
+  } else {
+  }
+}
+
+var x = window.matchMedia("(max-width: 720px)");
+myFunction(x);
+x.addListener(myFunction);
+*/
---
+++ vragen.md
@@ -0,0 +1,4 @@
+Vragen:
+
+Hoe kan ik de profielfoto vergroten?
+Hoe kan ik de nav laten verkleinen bij scrollen?
