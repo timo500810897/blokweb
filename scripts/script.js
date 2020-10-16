7b59cdbb66f84243324fc65b552ce5103b441f61
timo500810897 <70717768+timo500810897@users.noreply.github.com>
2020-10-16 16:02:29 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,23 @@ var x = window.matchMedia("(max-width: 720px)");
 myFunction(x);
 x.addListener(myFunction);
 */
+
+function toggleDropdown() {
+  var x = document.getElementById("toggleDropdown");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function toggleSearch() {
+  var x = document.getElementById("search");
+  
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
